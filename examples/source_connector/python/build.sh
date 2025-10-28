ce5839e205a1f4580d3d80e7cf35958b6b0d883c
Rishabh Ghosh <105623976+fivetran-rishabhghosh@users.noreply.github.com>
2025-10-28 22:27:44 +0530

    python source

--- examples/source_connector/python/build.sh
+++ examples/source_connector/python/build.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
-set -e  # Exit immediately on error
-
-# Use Python 3.11 for venv
-python3.11 -m venv connector_run
+# Creates virtual environment to install the packages and to run the connector.
+python3 -m venv connector_run
 source connector_run/bin/activate
+# install the added packages
+pip install -r requirements.txt
 
-# Upgrade base tools
-python -m pip install --upgrade pip setuptools wheel
-
-# Install dependencies
-python -m pip install -r requirements.txt
-
-# Copy protos
+# copying protos present in the root of directory to `protos` folder
 mkdir -p protos
 cp ../../../*.proto protos/
-
-# Generate gRPC stubs
+# Generates the required gRPC Python files using protos into `sdk_pb2` folder
 mkdir -p sdk_pb2
-
-# Use the exact venv Python binary to run grpc_tools
-./connector_run/bin/python -m grpc_tools.protoc \
-    --proto_path=./protos/ \
-    --python_out=sdk_pb2 \
-    --pyi_out=sdk_pb2 \
-    --grpc_python_out=sdk_pb2 protos/*.proto
-
-#deactivate
+python -m grpc_tools.protoc \
+       --proto_path=./protos/ \
+       --python_out=sdk_pb2 \
+       --pyi_out=sdk_pb2 \
+       --grpc_python_out=sdk_pb2 protos/*.proto
+deactivate
